aa001dd3f0d6ff7baf03f80af24f35f13c353b8a
Julien Borgeon <112864875+julienborgeon@users.noreply.github.com>
2026-09-12 00:47:20 +0200

    Add snippets and fix some typo

--- slides/assets/philosophy/notions.js
+++ slides/assets/philosophy/notions.js
@@ -33,7 +33,10 @@
           class="marpup-notion-dialog__close"
           aria-label="Fermer la définition"
         >
-          x
+          <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24" fill="currentColor">
+	          <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+	          <path d="M6.707 5.293l5.293 5.292l5.293 -5.292a1 1 0 0 1 1.414 1.414l-5.292 5.293l5.292 5.293a1 1 0 0 1 -1.414 1.414l-5.293 -5.292l-5.293 5.292a1 1 0 1 1 -1.414 -1.414l5.292 -5.293l-5.292 -5.293a1 1 0 0 1 1.414 -1.414" />
+          </svg>
         </button>
       </div>
 
